88215dded405d1d438b6897a4b8a9dbd6827915d
Mnglei <edward.munguulei@gmail.com>
2021-03-14 23:53:32 +0800

    4

--- app.js
+++ app.js
@@ -46,7 +46,18 @@ var financeController = (function () {
 
   return {
     addItem: function (type, desc, val) {
-      console.log("item added");
+      var item, id;
+
+      if (data.items[type].length === 0) id = 1;
+      else id = data.items[type][data.items[type].length - 1].id + 1;
+
+      if (type === "inc") item = new Income(id, desc, val);
+      else item = new Expense(id, desc, val);
+
+      data.items[type].push(item);
+    },
+    data: function () {
+      return data;
     },
   };
 })();
@@ -57,6 +68,7 @@ var appController = (function (uiController, financeController) {
     var input = uiController.getInput();
     //ugugdluudee sanhuugiin controller luu damjuulna
     financeController.addItem(input.type, input.description, input.value);
+
     //ugugdliig tohiroh hesegt gargana
     //tusviig tootsoh
     //etssiin uldegdel tootsoog delgetsend gargana

20040f59587a725d9c79652b9d069d3ca3f53f7c
Mnglei <edward.munguulei@gmail.com>
2021-03-23 02:20:18 +0800

    7

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var uiController = (function () {
     addBtn: ".add__btn",
     incomeList: ".income__list",
     expenseList: ".expenses__list",
+    tusuvLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expenseLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage",
+    containerDiv: ".container",
   };
   return {
     getInput: function () {
@@ -31,6 +36,23 @@ var uiController = (function () {
       fieldsArr[0].focus();
     },
 
+    tusviigUzuuleh: function (tusuv) {
+      document.querySelector(DOMstrings.tusuvLabel).textContent = tusuv.tusuv;
+      document.querySelector(DOMstrings.incomeLabel).textContent =
+        tusuv.totalInc;
+      document.querySelector(DOMstrings.expenseLabel).textContent =
+        tusuv.totalExp;
+      if (tusuv.huvi === 0)
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuv.huvi;
+      else
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuv.huvi + "%";
+    },
+    deleteListItem: function (id) {
+      var el = document.getElementById(id);
+      el.parentNode.removeChild(el);
+    },
     addListItem: function (item, type) {
       //inc, exp element beldene
       var html, list;
@@ -41,7 +63,7 @@ var uiController = (function () {
       } else {
         list = DOMstrings.expenseList;
         html =
-          '<div class="item clearfix" id="inc-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
+          '<div class="item clearfix" id="exp-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
       //orlogo zarlagiin utguudiig replace eer hiij ugnu
       html = html.replace("%id%", item.id);
@@ -66,6 +88,14 @@ var financeController = (function () {
     this.value = value;
   };
 
+  var calculateTotal = function (type) {
+    var sum = 0;
+    data.items[type].forEach((el) => {
+      sum += el.value;
+    });
+    data.totals[type] = sum;
+  };
+
   var data = {
     items: {
       inc: [],
@@ -76,9 +106,34 @@ var financeController = (function () {
       inc: 0,
       exp: 0,
     },
+    tusuv: 0,
+    huvi: 0,
   };
 
   return {
+    tusuvTootsooloh: function () {
+      calculateTotal("inc");
+      calculateTotal("exp");
+      data.tusuv = data.totals.inc - data.totals.exp;
+      data.huvi = Math.round((data.totals.exp / data.totals.inc) * 100);
+    },
+    deleteItem: function (type, id) {
+      var ids = data.items[type].map((el) => {
+        return el.id;
+      });
+      var index = ids.indexOf(id);
+      if (index !== -1) {
+        data.items[type].splice(index, 1);
+      }
+    },
+    tusviigAvah: function () {
+      return {
+        tusuv: data.tusuv,
+        huvi: data.huvi,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+      };
+    },
     addItem: function (type, desc, val) {
       var item, id;
 
@@ -114,10 +169,20 @@ var appController = (function (uiController, financeController) {
       //ugugdliig tohiroh hesegt gargana
       uiController.addListItem(item, input.type);
       uiController.clearFields();
-      //tusviig tootsoh
-      //etssiin uldegdel tootsoog delgetsend gargana
+
+      updateTusuv();
     }
   };
+
+  var updateTusuv = function () {
+    //tusviig tootsoh
+    financeController.tusuvTootsooloh();
+    //etssiin uldegdel tootsoog delgetsend gargana
+    var tusuv = financeController.tusviigAvah();
+    //tusviig delgetsend gargana
+    uiController.tusviigUzuuleh(tusuv);
+  };
+
   var setupEventListeners = function () {
     var DOM = uiController.getDOMstrings();
     document.querySelector(DOM.addBtn).addEventListener("click", function () {
@@ -128,11 +193,34 @@ var appController = (function (uiController, financeController) {
         ctrlAddItem();
       }
     });
+
+    document
+      .querySelector(DOM.containerDiv)
+      .addEventListener("click", function (event) {
+        var id = event.target.parentNode.parentNode.parentNode.parentNode.id;
+        if (id) {
+          var arr = id.split("-");
+          var type = arr[0];
+          var itemId = parseInt(arr[1]);
+
+          financeController.deleteItem(type, itemId);
+
+          uiController.deleteListItem(id);
+
+          updateTusuv();
+        }
+      });
   };
 
   return {
     init: function () {
       console.log("Application started...");
+      uiController.tusviigUzuuleh({
+        tusuv: 0,
+        huvi: 0,
+        totalInc: null,
+        totalExp: null,
+      });
       setupEventListeners();
     },
   };
